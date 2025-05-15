401ea7a7cb9a402526ce12f714ed092fc3c0ec91
Andrew Platov <andrey.pshenichny1998@gmail.com>
2025-05-15 14:21:10 +0300

    Починил отображение главной страницы

--- cityinfo/mainpage/urls.py
+++ cityinfo/mainpage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # главная страница
+    path('main/', views.index, name='main'),  # главная страница
     path('news/', views.news, name='news'),
     path('management/', views.management, name='management'),
     path('facts/', views.facts, name='facts'),
